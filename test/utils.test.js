3dbdb19efd26052c7e379019e3965d156cc5c655
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-05 15:49:02 -0800

    adjusted test a bit

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,33 +2,38 @@ import { renderAnimal, renderSoda, renderCar, renderSport, } from '../utils.js';
 
 const test = QUnit.test;
 
-test('renderAnimal should return a <div> with animals info', (expect) => {
+test('renderAnimal should return a <div> with animal info', (expect) => {
 
     const expected = `<div class="animals" id="animal-list"><div class="animal"><h2>DOG</h2><img src="./assets/mastiff.jpeg"><p>Mammal with 4 legs, Breed Mastiff.</p></div><div class="animal"><h2>CAT</h2><img src="./assets/siamese.jpeg"><p>Mammal with 4 legs, Breed Siamese.</p></div><div class="animal"><h2>DOG</h2><img src="./assets/retriever.jpeg"><p>Mammal with 4 legs, Breed Retriever.</p></div><div class="animal"><h2>SHARK</h2><img src="./assets/hammerhead.jpeg"><p>Fish with 0 legs, Breed Hammerhead.</p></div></div>`;
 
     const actual = renderAnimal({
-        // type: 'dog',
-        // numberOfLegs: 4,
-        // class: 'Mammal',
-        // breed: 'Mastiff',
-        // image: 'mastiff.jpeg',
+        type: 'SHARK',
+        numberOfLegs: 0,
+        class: 'Fish',
+        breed: 'Hammerhead',
+        image: 'hammerhead.jpeg',
     });
 
     expect.equal(actual.outerHTML, expected);
 });
 
 
-test('renderSoda should return a <div> with sodas info', (expect) => {
+stop('renderSoda should return a <div> with Mt Dew info', (expect) => {
 
     const expected = `<div class="soda" id="soda-list"><div class="soda"><h2>ROYAL COLA</h2><p>Royal Cola has a 175 calories and 45g of sugar.</p></div><div class="soda"><h2>MT DEW</h2><p>Mt Dew has a 400 calories and 70g of sugar.</p></div><div class="soda"><h2>PEPSI</h2><p>Pepsi has a 175 calories and 60g of sugar.</p></div><div class="soda"><h2>SQUIRT</h2><p>Squirt has a 120 calories and 40g of sugar.</p></div></div>`;
 
     const actual = renderSoda({
-    //     name:'Pepsi',
+
+    //     { name:'MT DEW',
     //     nutrition: {
-    //         calories: 175,
-    //         sugar: '60g',
-    //     }
+    //         calories: 400,
+    //         sugar: '70g',
+    //     },
+    // },
+    
+        
     });
+
     expect.equal(actual.outerHTML, expected);
 });
 
@@ -37,12 +42,13 @@ test('renderSoda should return a <div> with sodas info', (expect) => {
 
 
 
-test('renderCar should return a <div> with cars info', (expect) => {
+stop('renderCar should return a <div> with honda info', (expect) => {
 
     const expected = `<<div class="cars" id="cars-list"><div class="car"><h2>TOYOTA</h2><p>Corolla,Tundra,Camry .</p></div><div class="car"><h2>SUBARU</h2><p>Impreza,Wrx,Outback .</p></div><div class="car"><h2>FORD</h2><p>Mustang,Bronco,Focus .</p></div><div class="car"><h2>HONDA</h2><p>Accord,Pilot,Civic .</p></div></div>`;
 
     const actual = renderCar({
-    
+        // brand: 'honda',
+        // model:['Accord', 'Pilot', 'Civic']
     });
     expect.equal(actual.outerHTML, expected);
 });
@@ -50,12 +56,14 @@ test('renderCar should return a <div> with cars info', (expect) => {
 
 
 
-test('renderSport should return a <div> with sports info', (expect) => {
+stop('renderSport should return a <div> with baseball info', (expect) => {
 
     const expected = `<div class="sport" id="sport-list"><h2>Sports</h2><div class="sport"><p>Baseball .</p></div><div class="sport"><p>Hockey .</p></div><div class="sport"><p>Soccer .</p></div><div class="sport"><p>Tennis .</p></div></div>`; 
 
     const actual = renderSport({
+        // 'Baseball'
     
     });
+    
     expect.equal(actual.outerHTML, expected);
 });
