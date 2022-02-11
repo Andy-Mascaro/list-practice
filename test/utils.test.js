e1be8cec9062b2ff2dbfb9b8dbc8f318919af767
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-11 15:14:18 -0800

    added all tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,4 +1,4 @@
-import { renderAnimal } from '../utils.js';
+import { renderAnimal, renderCar, renderSoda, renderSport } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -12,6 +12,55 @@ test('renderAnimal should return a <div> with shark info', (expect) => {
         class: 'Fish',
         breed: 'Hammerhead',
         image: 'hammerhead.jpeg',
+    });
+
+    expect.equal(actual.outerHTML, expected);
+});
+
+
+
+
+
+test('renderSoda should return a <div> with soda info', (expect) => {
+
+    const expected = `<div class="soda"><h2>ROYAL COLA</h2><p>Royal Cola has a 175 calories and 45g of sugar.</p></div>`;
+
+    const actual = renderSoda({
+        name:'Royal Cola',
+        nutrition: {
+            calories: 175,
+            sugar: '45g',
+        }
+    });
+
+    expect.equal(actual.outerHTML, expected);
+});
+
+
+
+
+
+test('renderSport should return a <div> with sport info', (expect) => {
+
+    const expected = `<div class="sport"><p>Baseball .</p></div>`;
+
+    const actual = renderSport('Baseball');    
+    
+
+    expect.equal(actual.outerHTML, expected);
+});
+
+
+
+
+test('renderCars should return a <div> with toyota info', (expect) => {
+
+    const expected = `<div class="car"><h2>TOYOTA</h2><p>Corolla,Tundra,Camry .</p></div>`;
+
+    const actual = renderCar({
+        brand:'toyota',
+        model:['Corolla', 'Tundra', 'Camry']
+        
     });
 
     expect.equal(actual.outerHTML, expected);
